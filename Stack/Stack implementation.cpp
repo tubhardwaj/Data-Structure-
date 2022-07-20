af7a2274be27d31fd4b19b7e8406e94a028b5046
Tushar Bhardwaj <60889841+tubhardwaj@users.noreply.github.com>
2022-07-20 15:24:19 -0500

    Add files via upload

---
+++ Stack/Stack implementation.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+#define n 100
+
+class stack{
+	private:
+		int* arr;
+		int top;
+	public:
+		stack(){
+			arr = new int[n];
+			top= -1;
+		}
+		
+		void push(int x){
+			if(top==n-1){
+				cout<<"stack overflow"<<endl;
+				return;
+			}
+			
+			top++;
+			arr[top]= x;
+		}
+		void pop(){
+			if(top==-1){
+				cout<<"no element to pop"<<endl;
+				return;
+			}
+			top--;
+		}
+		int Top(){
+			if(top==-1){
+				cout<<"No element in Stack"<<endl;
+				return -1;
+			}
+			return arr[top];
+		}
+		bool empty(){
+			return top==-1;
+		}
+};
+
+int main(){
+	stack st;
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	cout<<st.Top()<<endl;
+	st.pop();
+	cout<<st.Top()<<endl;
+	st.pop();
+	st.pop();
+	st.pop();
+	cout<<st.empty()<<endl;
+}
